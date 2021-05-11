18fe29941caaf9ef485915706ec8419de91be4c6
Jessé Pereira de Souza Toledo <jesse_hacker@hotmail.com>
2021-05-11 18:37:56 -0300

    docker-compose

---
+++ EasyNetQ.RabbitMQ.Api/Controllers/MessagingController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace EasyNetQ.RabbitMQ.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MessagingController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+    }
+}
