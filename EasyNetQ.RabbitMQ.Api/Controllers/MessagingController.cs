64643c0384e4e41c770ab23afc75119be092ef5f
Jessé Pereira de Souza Toledo <jesse_hacker@hotmail.com>
2021-05-13 03:41:18 -0300

    Status Code Result

--- EasyNetQ.RabbitMQ.Api/Controllers/MessagingController.cs
+++ EasyNetQ.RabbitMQ.Api/Controllers/MessagingController.cs
@@ -1,8 +1,10 @@
 ﻿using EasyNetQ.RabbitMQ.Domain.Publish;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace EasyNetQ.RabbitMQ.Api.Controllers
 {
@@ -20,7 +22,7 @@ namespace EasyNetQ.RabbitMQ.Api.Controllers
         }
 
         [HttpPost]
-        public async void Post(MessageAvailable message, CancellationToken cancellationToken)
+        public async Task<IActionResult> Post(MessageAvailable message, CancellationToken cancellationToken)
         {
             _logger.LogInformation($"Entering {nameof(Post)}");
 
@@ -29,11 +31,13 @@ namespace EasyNetQ.RabbitMQ.Api.Controllers
                 await _pub.PublishAsync(message, cancellationToken).ConfigureAwait(false);
 
                 _logger.LogInformation($"Message: {message.Text}");
+
+                return new OkResult();
             }
             catch (Exception ex)
             {
                 _logger.LogError(ex, ex.Message);
-                throw;
+                return new StatusCodeResult(StatusCodes.Status500InternalServerError);
             }
         }
     }
